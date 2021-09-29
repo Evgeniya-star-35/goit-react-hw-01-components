44c35dca45ea049dcdcbbbc1f501c7f357d1bbbf
Evgeniya Bayda <baje75128@gmail.com>
2021-09-29 17:49:10 +0300

    prettierrc

--- src/components/friends/friendsList.js
+++ src/components/friends/friendsList.js
@@ -1,25 +1,25 @@
-import s from "./Friend.module.css";
-import PropTypes from "prop-types";
+import s from './Friend.module.css';
+import PropTypes from 'prop-types';
 
 function friendIsOnline(status) {
   if (status) {
-    return "#2ECC71";
+    return '#2ECC71';
   } else {
-    return "#E74C3C";
+    return '#E74C3C';
   }
 }
 export default function FriendsList({ friends }) {
   return (
     <div className={s.friends}>
       <ul className={s.friendList}>
-        {friends.map((friend) => (
+        {friends.map(friend => (
           <li className={s.item} key={friend.id}>
-            {" "}
+            {' '}
             <span
               className={s.status}
               style={{ color: friendIsOnline(friend.isOnline) }}
             >
-              {" "}
+              {' '}
               ●
             </span>
             <img
@@ -42,9 +42,10 @@ FriendsList.propTypes = {
   isOnline: PropTypes.bool.isRequired,
   id: PropTypes.number,
 };
+
 FriendsList.defaultProps = {
   avatar:
-    "https://st.depositphotos.com/1779253/5140/v/600/depositphotos_51402215-stock-illustration-male-avatar-profile-picture-use.jpg",
-  name: "User name",
+    'https://st.depositphotos.com/1779253/5140/v/600/depositphotos_51402215-stock-illustration-male-avatar-profile-picture-use.jpg',
+  name: 'User name',
   isOnline: false,
 };
