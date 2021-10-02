457813c27ca28081f939954370607631e97754c0
Evgeniya Bayda <baje75128@gmail.com>
2021-10-02 14:07:24 +0300

    delete comments

--- src/components/friends/friendsList.js
+++ src/components/friends/friendsList.js
@@ -19,9 +19,6 @@ export default function FriendsList({ friends }) {
 FriendsList.propTypes = {
   friends: PropTypes.arrayOf(
     PropTypes.shape({
-      // avatar: PropTypes.string.isRequired,
-      // name: PropTypes.string.isRequired,
-      // isOnline: PropTypes.bool.isRequired,
       id: PropTypes.number.isRequired,
     }),
   ),
